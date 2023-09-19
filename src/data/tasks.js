ba4a485397e48a3fecb0e5bbe5fca45dcf3aede6
AxelB <axelbork@outlook.com>
2023-09-19 00:35:40 -0300

    19/09/23 V 1.0

---
+++ src/data/tasks.js
@@ -0,0 +1,24 @@
+export const tasks = [
+    {
+        "id": 0,
+        "title": "🛒 Ir de compras",
+        "description": "Comprar alimentos y productos necesarios."
+    },
+    {
+        "id": 1,
+        "title": "🧹 Limpiar la casa",
+        "description": "Realizar la limpieza de todas las habitaciones y áreas comunes."
+    },
+    {
+        "id": 2,
+        "title": "📚 Estudiar",
+        "description": "Dedicar tiempo al estudio de temas específicos o tareas académicas."
+    },
+    {
+        "id": 3,
+        "title": "🍳 Cocinar",
+        "description": "Preparar una comida deliciosa para el día."
+    }
+]
+
+
